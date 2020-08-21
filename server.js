4082709393055c38cb47bd60fdebaf83fe7be804
Srishti <srishti.vashisht@outlook.com>
2020-08-22 01:40:23 +0530

    Second

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require("Express");   
+const { response } = require("Express");
 const app = express();               /*express() Creates express application*/
 
 
 /*Sending back response to request made*/ 
-app.get("/",function(request,response){
+app.get("/",function(request,response){                                  /*targetting home route*/
 
     response.send("<h1>Hello, nice to have you here</h1>");
 });
 
-app.listen(3000,function(){
+app.get("/contact",function(request,response){                            /*targetting contact route*/
+    response.send("Connect me at :srishtivashisht52@gmail.com");
+});
+
+app.get("/about",function(request,response){
+    response.send("About us Page");
+});
+
+app.listen(3000,function(){                                      /*3000 is port here*/
     console.log("server started on port 3000");
 
 });
